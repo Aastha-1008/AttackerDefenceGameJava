cd1c14178706d12a50412382294766e8f8eed5cf
Aastha-1008 <aastha.chaudhary237@gmail.com>
2024-04-11 21:45:32 +0530

    added test case

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -6,8 +6,25 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
 
     @Test
-    public void attackingDefendingFight(){
+    public void attackingDefendingFight1(){
         Player playerA = new Player(50 , 5, 10);
+        Player playerB = new Player(100,10,5);
+
+        Game game = new Game(playerA , playerB);
+        game.attackingDefendingFight();
+
+        if(playerA.getHealth()<=0){
+            assertEquals(true , playerB.getHealth()>0);
+        }
+        else{
+            assertEquals(true,playerA.getHealth()>0);
+        }
+
+    }
+
+    @Test
+    public void attackingDefendingFight2(){
+        Player playerA = new Player(500 , 15, 10);
         Player playerB = new Player(100,10,5);
 
         Game game = new Game(playerA , playerB);
